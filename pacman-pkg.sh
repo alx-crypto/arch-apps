21af29266dbe6638fb64b6016926469ef0d0dab7
Alx <alxvss-crypto@protonmail.com>
2022-01-20 10:40:43 +0100

    c2

--- pacman-pkg.sh
+++ pacman-pkg.sh
@@ -4,9 +4,5 @@
 sudo pacman -Syu
 sudo pacman -S git libreoffice-fresh godot krita discord gimp latte-dock vim kate kvantum ktorrent mpv virt-manager ark gwenview dragon kcalc kdenlive okular spectacle kdeplasma-addons partitionmanager sweeper neofetch filelight elisa qt6 vlc firewalld wget cmatrix
 
-#AUR Package
-paru -S corectrl-git timeshift timeshift-autosnap librewolf-bin brave-bin kwin-bismuth-git
-paru -Syu
-
 #Reboot
 reboot
---
+++ paru-aur-pkg.sh
@@ -0,0 +1,8 @@
+# !bash/bin
+
+#AUR Package
+paru -S corectrl-git timeshift timeshift-autosnap librewolf-bin brave-bin kwin-bismuth-git
+paru -Syu
+
+#Reboot
+reboot
