d444d02062210b34c61ad4fbd30460a5e3a6548f
alx-crypto <89910051+alx-crypto@users.noreply.github.com>
2022-01-23 12:59:10 +0100

    Update yay-aur-pkg.sh

--- yay-aur-pkg.sh
+++ yay-aur-pkg.sh
@@ -1,8 +1,5 @@
 # !bash/bin
 
 #AUR Package
-yay -S corectrl-git timeshift timeshift-autosnap librewolf-bin brave-bin kwin-bismuth-git
 yay -Syu
-
-#Reboot
-reboot
+yay -S corectrl-git timeshift timeshift-autosnap librewolf-bin brave-bin kwin-bismuth-git
