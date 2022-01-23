94e5bd01cfa034fc2f84e3c7a3883538ee62addf
alx-crypto <89910051+alx-crypto@users.noreply.github.com>
2022-01-23 12:48:02 +0100

    Update paru-aur-pkg.sh

--- paru-aur-pkg.sh
+++ paru-aur-pkg.sh
@@ -1,8 +1,5 @@
 # !bash/bin
 
 #AUR Package
-paru -S corectrl-git timeshift timeshift-autosnap librewolf-bin brave-bin kwin-bismuth-git
 paru -Syu
-
-#Reboot
-reboot
+paru -S corectrl-git timeshift timeshift-autosnap librewolf-bin brave-bin kwin-bismuth-git
