f6a5aa85d27c5f42e17c842a8ffa2f686c7ba952
alx-crypto <89910051+alx-crypto@users.noreply.github.com>
2022-01-23 12:47:37 +0100

    Update pacman-pkg.sh

--- pacman-pkg.sh
+++ pacman-pkg.sh
@@ -4,5 +4,3 @@
 sudo pacman -Syu
 sudo pacman -S git libreoffice-fresh godot krita discord gimp latte-dock vim kate kvantum ktorrent mpv virt-manager ark gwenview dragon kcalc kdenlive okular spectacle kdeplasma-addons partitionmanager sweeper neofetch filelight elisa qt6 vlc firewalld wget cmatrix archlinux-wallpaper atom
 
-#Reboot
-reboot
